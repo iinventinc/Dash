9f5dcbcd43418ee8eaeaeb188ce6db0ecd63a693
Mattias Lucas <mathiaseluma@gmail.com>
2021-09-13 19:50:43 +0100

    uploads

--- dash/lib/Screens/signup.dart
+++ dash/lib/Screens/signup.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -74,9 +75,10 @@ class SignUp extends StatelessWidget {
                   height: 50,
                   width: 400,
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2.0),
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20)),
+                    border: Border.all(color: Colors.black, width: 2.0),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                   child: Text(
                     "Sign up with Google",
                     style: TextStyle(
@@ -116,14 +118,29 @@ class SignUp extends StatelessWidget {
 
               SizedBox(height: 10),
 //ALLOW PERMISSION TO READ CONTACTS
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
+              SizedBox(
+                height: 55,
+                child: TextFormField(
+                  style: TextStyle(fontSize: 20.0),
+                  decoration: InputDecoration(
+                    hintText: 'Enter your mobile number',
+                    fillColor: Colors.grey[100],
+                    filled: true,
+                    labelStyle: GoogleFonts.roboto(fontSize: 24.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
                       borderRadius: BorderRadius.all(
                         Radius.circular(30),
                       ),
                     ),
-                    hintText: 'Enter your phone number'),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
               ),
 
               SizedBox(height: 100),
@@ -134,13 +151,13 @@ class SignUp extends StatelessWidget {
                   height: 50,
                   width: 400,
                   decoration: BoxDecoration(
-                      color: Colors.blue.shade900,
-                      borderRadius: BorderRadius.circular(30)),
+                    color: Colors.blue.shade900,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                   child: Text(
                     "Send Otp",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                    style:
+                        GoogleFonts.roboto(fontSize: 18.0, color: Colors.white),
                   ),
                 ),
                 onPressed: () {},
@@ -151,13 +168,14 @@ class SignUp extends StatelessWidget {
                   height: 50,
                   width: 400,
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 1.0),
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30)),
+                    border: Border.all(color: Colors.black, width: 1.0),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                   child: Text(
                     "Sign up with Google",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
+                    style:
+                        GoogleFonts.roboto(fontSize: 18.0, color: Colors.black),
                   ),
                 ),
                 onPressed: () {},
@@ -231,9 +249,7 @@ class SignUp extends StatelessWidget {
                     borderRadius: BorderRadius.circular(20)),
                 child: Text(
                   "Send Otp",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+                  style: GoogleFonts.courgette(fontSize: 18),
                 ),
               ),
               onPressed: () {},
@@ -244,14 +260,13 @@ class SignUp extends StatelessWidget {
                 height: 50,
                 width: 400,
                 decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2.0),
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
+                  border: Border.all(color: Colors.black, width: 2.0),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 child: Text(
                   "Sign up with Google",
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
+                  style: GoogleFonts.courgette(fontSize: 18.0),
                 ),
               ),
               onPressed: () {},
