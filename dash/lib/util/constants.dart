6acab17bcab9d836dd3bbaf0a37842c50ce44d3e
iinventinc <86780544+iinventinc@users.noreply.github.com>
2021-10-01 17:55:58 +0100

    added constants for the general color and style
    for the text and color

---
+++ dash/lib/util/constants.dart
@@ -0,0 +1,14 @@
+ import 'package:flutter/material.dart';
+
+
+ const TextStyle mystyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+     const TextStyle Whitestyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+
