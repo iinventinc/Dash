c5aeb07afdde1554dffab04a51a6b4b808a2e766
iinventinc <86780544+iinventinc@users.noreply.github.com>
2021-10-01 17:56:29 +0100

    i added more screens to the route

--- dash/lib/Routes/routes.dart
+++ dash/lib/Routes/routes.dart
@@ -1,7 +1,5 @@
-import 'package:dash/Screens/home.dart';
-import 'package:dash/Screens/login.dart';
-import 'package:dash/Screens/onboarding.dart';
-import 'package:dash/Screens/signup.dart';
+import '../Screens/exportscreen.dart';
+
 import 'package:flutter/material.dart';
 
 class RouteGenerator {
@@ -10,25 +8,77 @@ class RouteGenerator {
   static const String login = '/login';
   static const String signup = '/sign up';
   static const String otp = '/otp';
+  static const String payments = '/payment';
+  static const String discount = '/discount';
+
+  static const String orderride = '/orderride';
+  static const String ridehistory = '/ridehistory';
+  static const String customizeride = '/customizeride';
+  static const String customersupport = '/customersupport';
+  static const String about = '/about';
+  static const String dashdriver = '/dashdriver';
+  static const String profile = '/profile';
+  static const String legal = '/legal';
+  static const String edithome = '/edithome';
 
   RouteGenerator._() {}
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case onboarding:
-        return MaterialPageRoute(builder: (_) =>  Onboarding());
+        return MaterialPageRoute(builder: (_) => Onboarding());
 
       case home:
-        return MaterialPageRoute(builder: (_) =>  Home());
+        return MaterialPageRoute(builder: (_) => Home());
 
       case signup:
-        return MaterialPageRoute(builder: (_) =>  SignUp());
+        return MaterialPageRoute(builder: (_) => SignUp());
+      case discount:
+        return MaterialPageRoute(builder: (_) => Discount());
 
       case login:
         return MaterialPageRoute(builder: (_) => Login());
+      case orderride:
+        return MaterialPageRoute(builder: (_) => OrderRide());
 
+      //
+      case orderride:
+        return MaterialPageRoute(builder: (_) => OrderRide());
+
+      case ridehistory:
+        return MaterialPageRoute(builder: (_) => RideHistory());
+
+      case customizeride:
+        return MaterialPageRoute(builder: (_) => CustomizeRide());
+      case discount:
+        return MaterialPageRoute(builder: (_) => Discount());
+
+      case orderride:
+        return MaterialPageRoute(builder: (_) => OrderRide());
+      case payments:
+        return MaterialPageRoute(builder: (_) => Payments());
+      case edithome:
+        return MaterialPageRoute(builder: (_) => EditHome());
+
+      case customersupport:
+        return MaterialPageRoute(builder: (_) => CustomerSupport());
+
+      case about:
+        return MaterialPageRoute(builder: (_) => InviteFriends());
+      case profile:
+        return MaterialPageRoute(builder: (_) => Profile());
+      case legal:
+        return MaterialPageRoute(builder: (_) => Legal());
+
+      case dashdriver:
+        return MaterialPageRoute(builder: (_) => DashDriver());
       default:
         throw FormatException("Route does not exist");
     }
   }
+}
+
+class RouteException implements Exception {
+  final String message;
+  const RouteException(this.message);
 }
