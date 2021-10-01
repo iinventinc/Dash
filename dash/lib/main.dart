6820903b682d2e9a7f960cd7dfdff36c0cde2077
iinventinc <86780544+iinventinc@users.noreply.github.com>
2021-10-01 17:57:42 +0100

    this generated plugin was for the package
    in pubspec.yaml

--- dash/lib/main.dart
+++ dash/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      
       initialRoute: RouteGenerator.onboarding,onGenerateRoute: RouteGenerator.generateRoute,
       
     );
