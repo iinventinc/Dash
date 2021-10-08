1a91f0a923e6e21b6d72ebd4aa9cc2b019c7e03b
Mattias Lucas <mathiaseluma@gmail.com>
2021-10-08 15:23:23 +0100

    uploads

--- dash/lib/Screens/home.dart
+++ dash/lib/Screens/home.dart
@@ -49,76 +49,163 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        key: _drawerKey,
-        drawer: MyDrawer(),
-        body: SafeArea(
-          child: Container(
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                      icon: Icon(Icons.menu),
-                      iconSize: 30,
-                      splashColor: Colors.blue,
-                      color: Colors.black,
+    return Scaffold(
+      key: _drawerKey,
+      drawer: MyDrawer(),
+      body: Container(
+        width: double.infinity,
+        height: 800,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: _center, zoom: 11.0),
+              zoomControlsEnabled: false,
+              onMapCreated: _onMapCreated,
+            ),
+            Positioned(
+              top: 40,
+              right: 15,
+              left: 15,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
                       onPressed: () {
                         _showDrawer();
-                      }),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 600,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Stack(
-                    children: <Widget>[
-                      GoogleMap(
-                        initialCameraPosition:
-                            CameraPosition(target: _center, zoom: 11.0),
-                        zoomControlsEnabled: false,
-                        onMapCreated: _onMapCreated,
+                      },
+                      icon: Icon(Icons.menu),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        showCursor: true,
+                        readOnly: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Where does thou go? Hogwarts?'),
+                        cursorColor: Colors.black,
+                        onTap: () {
+                          showModalBottomSheet(
+                              isScrollControlled: true,
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  padding: EdgeInsets.only(
+                                      bottom: MediaQuery.of(context)
+                                          .viewInsets
+                                          .bottom),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Form(
+                                        child: Column(
+                                          children: <Widget>[
+                                            SizedBox(
+                                              height: 30,
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(15.0),
+                                              child: TextFormField(
+                                                decoration: InputDecoration(
+                                                  hintText:
+                                                      'No.2 nairobi street wuse',
+                                                  prefixIcon: Icon(
+                                                    Icons.location_on,
+                                                    color: Colors.orange,
+                                                  ),
+                                                  border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            12.0),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(15.0),
+                                              child: TextFormField(
+                                                decoration: InputDecoration(
+                                                  prefixIcon: Icon(
+                                                    Icons.explore_outlined,
+                                                    color: Colors.green,
+                                                  ),
+                                                  hintText:
+                                                      'search your destination',
+                                                  border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            12.0),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      ListTile(
+                                        leading: Icon(
+                                          Icons.home,
+                                          color: Colors.blue,
+                                        ),
+                                        title: Text(
+                                          'Add Home or Work Place',
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 18.0),
+                                        ),
+                                      ),
+                                      Divider(
+                                        color: Colors.black,
+                                      ),
+                                      ListTile(
+                                        leading: Icon(Icons.lock_clock),
+                                        title: Text(
+                                          'Recent',
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 18.0),
+                                        ),
+                                      ),
+                                      Divider(
+                                        color: Colors.black,
+                                      ),
+                                      ListTile(
+                                        leading: Icon(Icons.location_on),
+                                        title: Text(
+                                          'Gwarimpa 3rd Avenue',
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 18.0),
+                                        ),
+                                      ),
+                                      Divider(),
+                                      ListTile(),
+                                      Divider(
+                                        color: Colors.black,
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
                       ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        left: 15,
-                        child: Container(
-                          color: Colors.white,
-                          child: Row(
-                            children: <Widget>[
-                              IconButton(
-                                onPressed: () {
-                                  _showDrawer();
-                                },
-                                icon: Icon(Icons.menu),
-                              ),
-                              Expanded(
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText:
-                                          'Where does thou go? Hogwarts?'),
-                                  cursorColor: Colors.black,
-                                  onTap: () {},
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            )
+          ],
         ),
-        bottomNavigationBar: MyBottomNavBar(),
       ),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 }
