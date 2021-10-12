32cf7e40544bc66fc28fe54cf8b5fff5b999e4e9
Mattias Lucas <mathiaseluma@gmail.com>
2021-10-12 16:16:22 +0100

    uploads

--- dash/lib/generated_plugin_registrant.dart
+++ dash/lib/generated_plugin_registrant.dart
@@ -5,6 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:geolocator_web/geolocator_web.dart';
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -13,6 +14,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  GeolocatorPlugin.registerWith(registrar);
   GoogleMapsPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
