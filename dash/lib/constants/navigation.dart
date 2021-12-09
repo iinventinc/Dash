27ffcd8c7e37f76aecd3caa60f344a2236579f32
Mattias Lucas <mathiaseluma@gmail.com>
2021-12-09 14:07:24 +0100

    uploads

--- dash/lib/constants/navigation.dart
+++ dash/lib/constants/navigation.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
+void push(BuildContext context, Widget route) =>
+    Navigator.push(context, MaterialPageRoute(builder: (context) => route));
 
-void push(BuildContext context, Widget route) => Navigator.push(context, MaterialPageRoute(builder: (context) => route));
-
-void pop(BuildContext context) =>
-    Navigator.pop(context);
-
+void pop(BuildContext context) => Navigator.pop(context);
 
 void pushNamed(BuildContext context, String route) =>
     Navigator.pushNamed(context, route);
-
 
 void popAndPushNamed(BuildContext context, String route) =>
     Navigator.popAndPushNamed(context, route);
--- dash/lib/views/home/rides.dart
+++ dash/lib/views/home/rides.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Rides extends StatefulWidget {
-  const Rides({ Key? key }) : super(key: key);
+  const Rides({Key? key}) : super(key: key);
 
   @override
   _RidesState createState() => _RidesState();
 }
 
 class _RidesState extends State<Rides> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Icon(Icons.ac_unit),
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: _center,
+          ),
+          onMapCreated: _onMapCreated,
+        ),
+      ),
     );
   }
 }
